4a5ac75403b32bd1047478a91fc7578175367847
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-22 10:07:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 sudo apt -y update && sudo apt -y install build-essential libssl-dev libdb++-dev && sudo apt -y install libboost-all-dev libcrypto++-dev libqrencode-dev && sudo apt -y install libminiupnpc-dev libgmp-dev libgmp3-dev autoconf && sudo apt -y install autogen automake libtool autotools-dev pkg-config && sudo apt -y install bsdmainutils software-properties-common && sudo apt -y install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev && sudo add-apt-repository ppa:bitcoin/bitcoin -y && sudo apt-get update && sudo apt-get install libdb4.8-dev libdb4.8++-dev -y && sudo apt-get install unzip -y && sudo apt-get install -y pwgen
 cd /usr/local/bin
-wget http://github.com/axsgold/axs/releases/download/1.0.0.1-DAEMON/axs-daemon-precompiled.zip
-unzip axs-daemon-precompiled.zip
+wget https://github.com/axsgold/axs/releases/download/v2.0.0.1-FORK/axsd-precompiled-daemon-v2.zip
+unzip axsd-precompiled-daemon-v2.zip
 chmod -R 755 axsd
-rm axs-daemon-precompiled.zip
+rm axsd-precompiled-daemon-v2.zip
 cd
 mkdir /root/.axs
 chmod -R 755 /root/.axs
 GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
-echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=95.179.151.40:33771\naddnode=94.231.78.31:33771\naddnode=80.211.159.202\naddnode=77.81.230.241\naddnode=45.32.232.182" > /root/.axs/axs.conf
+echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=209.250.251.252:33771\naddnode=217.69.0.8:33771\naddnode=209.250.239.117\naddnode=45.76.133.189:33771\naddnode=207.148.115.228:33771" > /root/.axs/axs.conf
 name=axs
 daemon=axsd
 cat << EOF | sudo tee /etc/systemd/system/axs@root.service
