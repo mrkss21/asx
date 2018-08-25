e4be731fe6aa1ffdbf32207c59985cd689e92cfb
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-25 11:21:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ unzip axs-v4-precompiled-daemon.zip
 chmod -R 755 axsd
 rm axs-v4-precompiled-daemon.zip
 cd
-mkdir /root/.axs
-chmod -R 755 /root/.axs
+mkdir /root/.axs4
+chmod -R 755 /root/.axs4
 GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
-echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=95.179.132.198:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711\naddnode=95.179.132.198:51711\naddnode=35.196.209.18:51711\naddnode=94.177.163.52:51711\naddnode=80.211.89.32:51711\naddnode=176.120.186.186:51711\naddnode=46.146.233.115:51711\naddnode=213.183.51.230:51711\naddnode=45.63.109.44:51711\naddnode=209.250.236.24:51711\naddnode=95.171.6.57:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711" > /root/.axs/axs.conf
+echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=95.179.132.198:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711\naddnode=95.179.132.198:51711\naddnode=35.196.209.18:51711\naddnode=94.177.163.52:51711\naddnode=80.211.89.32:51711\naddnode=176.120.186.186:51711\naddnode=46.146.233.115:51711\naddnode=213.183.51.230:51711\naddnode=45.63.109.44:51711\naddnode=209.250.236.24:51711\naddnode=95.171.6.57:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711" > /root/.axs4/axs.conf
 name=axs
 daemon=axsd
 cat << EOF | sudo tee /etc/systemd/system/axs@root.service
@@ -30,7 +30,7 @@ sudo systemctl start axs@$USER
 sleep 10
 masternodekey=$(axsd masternode genkey)
 axsd stop
-echo -e "masternode=1\nmasternodeprivkey=$masternodekey" >> /root/.axs/axs.conf
+echo -e "masternode=1\nmasternodeprivkey=$masternodekey" >> /root/.axs4/axs.conf
 axsd -daemon
 echo ""
 echo "Your Masternode IP address: ${IP_ADD}:33771"
