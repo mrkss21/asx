85b24e50c6dc0e6ad9e9475f03f5da830fb64e22
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-25 10:48:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 sudo apt -y update && sudo apt -y install build-essential libssl-dev libdb++-dev && sudo apt -y install libboost-all-dev libcrypto++-dev libqrencode-dev && sudo apt -y install libminiupnpc-dev libgmp-dev libgmp3-dev autoconf && sudo apt -y install autogen automake libtool autotools-dev pkg-config && sudo apt -y install bsdmainutils software-properties-common && sudo apt -y install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev && sudo add-apt-repository ppa:bitcoin/bitcoin -y && sudo apt-get update && sudo apt-get install libdb4.8-dev libdb4.8++-dev -y && sudo apt-get install unzip -y && sudo apt-get install -y pwgen
 cd /usr/local/bin
-wget https://github.com/axsgold/axs/releases/download/v2.0.0.1-FORK/axsd-precompiled-daemon-v2.zip
-unzip axsd-precompiled-daemon-v2.zip
+wget https://github.com/axsgold/axs/releases/download/v4.0.0.2/axs-v4-precompiled-daemon.zip
+unzip axs-v4-precompiled-daemon.zip
 chmod -R 755 axsd
-rm axsd-precompiled-daemon-v2.zip
+rm axs-v4-precompiled-daemon.zip
 cd
 mkdir /root/.axs
 chmod -R 755 /root/.axs
 GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
-echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=209.250.251.252:33771\naddnode=217.69.0.8:33771\naddnode=209.250.239.117\naddnode=45.76.133.189:33771\naddnode=207.148.115.228:33771" > /root/.axs/axs.conf
+echo -e "rpcuser=axsrpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=64\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}\naddnode=95.179.132.198:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711\naddnode=95.179.132.198:51711\naddnode=35.196.209.18:51711\naddnode=94.177.163.52:51711\naddnode=80.211.89.32:51711\naddnode=176.120.186.186:51711\naddnode=46.146.233.115:51711\naddnode=213.183.51.230:51711\naddnode=45.63.109.44:51711\naddnode=209.250.236.24:51711\naddnode=95.171.6.57:51711\naddnode=45.63.121.133:51711\naddnode=66.42.49.66:51711" > /root/.axs/axs.conf
 name=axs
 daemon=axsd
 cat << EOF | sudo tee /etc/systemd/system/axs@root.service
